8a3ba65ed9b062921aaeaaa533a82036cda142c2
Aman Madaan <aman@aman-VPCEB14EN.(none)>
2013-07-28 22:37:47 +0530

    Added the function to return probability of a word using the bigram model.

--- pc/pc.cpp
+++ pc/pc.cpp
@@ -16,7 +16,7 @@ class BigramProbModel
 
     mpf_class pc[26][26]; //to store pc('j'|'i')
     public:
-    BigramProb(string bigramFile)
+    BigramProbModel(string bigramFile)
     {
         mpf_class count, total;
         total = 0;
@@ -36,8 +36,8 @@ class BigramProbModel
             for(int j = 0; j < 26; j++) { //calculate the total and store it
                 fi >> temp;
                 fi >> count;
-            pc[i][j] = count;
-            total = total + count;
+                pc[i][j] = count;
+                total = total + count;
             }
             //now we have total words that start with 'a'
             //we also have in each pc[0][j] the count of words that 
@@ -50,23 +50,42 @@ class BigramProbModel
         }
     }
 
-    double prob(string bigram)
+    //for a bigram b1b2, returns the prob of b2 given b1
+    double biProb(string bigram)
     {
         return pc[bigram[0] - 97][bigram[1] - 97].get_d();
     }
-};
 
+    //returns the probability of a word occurring by breaking it into bigrams 
+    double wordProb(string word)
+    {
+        int l = word.length();
+        double res = 1;
+        for(int i = 0; i < l - 1; i++) {
+            //cout << word.substr(i, 2) << " = " << biProb(word.substr(i, 2)) <<  endl;
+            res = res * biProb(word.substr(i, 2));//for each bigram
+        }
+        return res;
+    }
+};
+/*The tester for BigramProbabilityModel
+ *
+ * LEAVE COMMENTED 
+ *
 int main()
 {
     string temp;
-    BigramProb bp("../data/bicount.txt");
+    BigramProbModel bp(string("../data/bicount.txt"));
     cout << "> ";
     cin >> temp;
     while(temp != "ex") {
-        cout << "P (" << temp[1] << " | " << temp[0] <<") = " << bp.prob(temp) << endl;
-    cout << "> ";
+     //       cout << "P (" << temp[1] << " | " << temp[0] <<") = " << bp.prob(temp) << endl;
+    
+        cout << bp.wordProb(temp) << endl;
+        cout << "> ";
     cin >> temp;
     }
     return 0;
 
 }
+*/
