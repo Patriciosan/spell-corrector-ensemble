264aa6859ada3af5be1061eb331e0edf382b5bae
Aman Madaan <aman@aman-VPCEB14EN.(none)>
2013-08-07 18:23:02 +0530

    Finished the matrices. UNTESTED

--- jurafsky/subsmat.py
+++ jurafsky/subsmat.py
@@ -10,6 +10,7 @@ correctionFile = open("kc.txt", "r")
 subs = [[0 for i in range(26)] for j in range(26)]
 trans = [[0 for i in range(26)] for j in range(26)]
 de = [[0 for i in range(26)] for j in range(26)]
+ins = [[0 for i in range(26)] for j in range(26)]
 
 for correctionEntry in correctionFile:
 
@@ -42,18 +43,39 @@ for correctionEntry in correctionFile:
                         trans[ri][ci] += 1
 
         elif(len(ce[1]) > len(ce[0])): #correct word is longer, deletion happened
-              i = 0
+            '''i,j element of the deletion matrix dl stores the number of 
+            times i was written instead of ij'''
+            i = 0
+            #skip the common prefix
+            while( i < len(ce[0]) and ce[0][i] == ce[1][i]):
+                i += 1
+                #in *any* case, ce[1][i] now refers to the deleted character
+                ri = ord(ce[0][i - 1]) - 97 #row index
+                ci = ord(ce[1][i]) - 97 #column index
+                '''now i points to the character that was deleted
+                so, ce[0][i - 1] was written instead of ce[1][i - 1],ce[1][i]'''
+                if(ri >= 0 and ri <= 25 and ci >= 0 and ci <= 25):
+                    de[ri][ci] += 1
+        
+        else: #length of incorrect word is longer than correct word, insertion
+            '''i,j element of the deletion matrix dl stores the number of 
+            times i was written instead of ij'''
+            i = 0
               #skip the common prefix
               
-              while( i < len(ce[0]) and ce[0][i] == ce[1][i]):
-                  i += 1
-              #in *any* case, ce[1][i] now refers to the deleted character
-              ri = ord(ce[0][i - 1]) - 97 #row index
-              ci = ord(ce[1][i]) - 97 #column index
-              '''now i points to the character that was deleted
-              so, ce[0][i - 1] was written instead of ce[1][i - 1],ce[1][i]'''
-              if(ri >= 0 and ri <= 25 and ci >= 0 and ci <= 25):
-                de[ri][ci] += 1
+            while( i < len(ce[1]) and ce[0][i] == ce[1][i]):
+                i += 1
+                '''in *any* case, ce[0][i] now refers to the inserted character
+                We should have typed ce[0][i - 1] only, but we have written ce[0][i]
+                '''
+                ri = ord(ce[0][i - 1]) - 97 #row index
+                ci = ord(ce[0][i]) - 97 #column index
+                '''now i points to the character that was deleted
+                so, ce[0][i - 1] was written instead of ce[1][i - 1],ce[1][i]'''
+                if(ri >= 0 and ri <= 25 and ci >= 0 and ci <= 25):
+                    ins[ri][ci] += 1
+        
+ 
 
 
 
@@ -64,4 +86,5 @@ for i in range(26):
         print trans[i]
 for i in range(26):
         print de[i]
-
+for i in range(26):
+        print ins[i]
